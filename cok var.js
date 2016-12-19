a7abc1f13352ce428c2d302ef7e2bf519cd0b62f
Muhamad Firdaus Hidayat <ifirdausku@gmail.com>
2016-12-19 13:12:58 +0700

    Cok var

---
+++ cok var.js
@@ -0,0 +1,4 @@
+<script>
+var cok = document.getElementById('ayPrem').contentWindow.document;cok.open();
+cok.write(unescape('%3Cimg%20border%3D%220%22%20src%3D%22http%3A%2F%2F1.bp.blogspot.com%2F-yATYJx6rBjU%2FVUsmKgvsJWI%2FAAAAAAAACoQ%2FR0GzR3GI8Zs%2Fs1600%2Ffree-wifi.png%22%20onmousedown%3D%22return%20false%22%20oncontextmenu%3D%22return%20false%22%20onkeydown%3D%22return%20false%22%20alt%3D%22Nulis%20ndiri%20bro%20%3A-P%22%20%2F%3E%3C%2Fa%3E'));cok.close();
+</script>
